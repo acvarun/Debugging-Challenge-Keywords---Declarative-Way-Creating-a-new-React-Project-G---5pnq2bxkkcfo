c8bd3d3252a847975ffe73eb331cb6fb5845310b
acvarun <96919265+acvarun@users.noreply.github.com>
2022-12-16 18:46:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,17 @@ const App = () => {
 
   let [count, setCount] = useState(0)
 
+  function click(){
+    setCount(count + 1);
+  }
+  function doubleClick(){
+    alert("cant edit it") 
+    setCount(count-2);
+  }
   return (
     <div className="ball">
       <h1 className="count">{count}</h1>
-      <button className='increment-button' onClick={() => { setCount(count + 1) }} onDoubleClick={() => { alert("cant edit it") }}>Increment</button>
+      <button className='increment-button' onClick={click} onDoubleClick={doubleClick}>Increment</button>
     </div>
   )
 }
-
-
-export default App;
